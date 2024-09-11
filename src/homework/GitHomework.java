3e4853b1e3a63abe0f175c5d260d24d4cab15561
maysa1994 <mhatamova94@gmail.com>
2024-09-10 20:50:24 -0400

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -3,6 +3,9 @@ package homework;
 public class GitHomework {
 	public static void main (String [] args) {
 		System.out.println("Batch12: Git Class Lecture");
+		int age = 12;
+		String name = "John";
+		//this part is from github 
 	}
 
 }
